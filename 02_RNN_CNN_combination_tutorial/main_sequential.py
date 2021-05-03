d0ec443bddc6e7a6a4bb2f3ff68ee18df1ddd746
luwis93choi <luwis93choi@hotmail.com>
2021-05-03 21:35:05 +0900

    Editted to support image only dataloading

--- 02_RNN_CNN_combination_tutorial/main_sequential.py
+++ 02_RNN_CNN_combination_tutorial/main_sequential.py
@@ -23,61 +23,41 @@ batch_size = 1
 dataset = sequential_sensor_dataset(lidar_dataset_path=args['input_lidar_file_path'], 
                                     img_dataset_path=args['input_img_file_path'], 
                                     pose_dataset_path=args['input_pose_file_path'],
-                                    train_sequence=['00', '01', '02'], valid_sequence=['01'], test_sequence=['02'],
-                                    sequence_length=3)
+                                    train_sequence=['01', '02'], valid_sequence=['01'], test_sequence=['02'],
+                                    sequence_length=5)
 
 dataloader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=False, num_workers=0, drop_last=True)
 
-dataloader.dataset.mode = 'test'
-for batch_idx, (lidar_range_img_stack_tensor, current_img_tensor, pose_6DOF_tensor) in enumerate(dataloader):
+dataloader.dataset.mode = 'training'
+for batch_idx, (current_img_tensor, pose_6DOF_tensor) in enumerate(dataloader):
 
-    # Data Dimension Standard : Batch Size x Sequence Length x Data Shape
-    # Sequential LiDAR = Batch Size x Sequence Length x 68 (Height) x 1800 (Width)
-    # Sequential Image = Batch Size x Sequence Length x 3 (Channel) x 376 (Height) x 1241 (Width)
-    # Sequential Pose = Batch Size x Sequence Length x 6 (6 DOF)
+    if (current_img_tensor != []) and (pose_6DOF_tensor != []):
 
-    print(lidar_range_img_stack_tensor.size())
-    print(current_img_tensor.size())
-    print(pose_6DOF_tensor.size())
-    print('---------------------------------')
+        # Data Dimension Standard : Batch Size x Sequence Length x Data Shape
+        # Sequential Image = Batch Size x Sequence Length x 3 (Channel) x 376 (Height) x 1241 (Width)
+        # Sequential Pose = Batch Size x Sequence Length x 6 (6 DOF)
 
-    ### Sequential LiDAR Stack Display ###
-    lidar_img_sequence_list = []
-    lidar_total_img = []
-    seq_len = dataloader.dataset.sequence_length
-    for batch_index in range(lidar_range_img_stack_tensor.size(0)):
+        print(current_img_tensor.size())
+        print(pose_6DOF_tensor.size())
+        print('---------------------------------')
 
-        for seq in range(dataloader.dataset.sequence_length):
-            lidar_range_img = np.array(TF.to_pil_image(lidar_range_img_stack_tensor[batch_index][seq]))
-            lidar_range_img = cv.resize(lidar_range_img, dsize=(int(1280/seq_len), int(240/(seq_len * 0.5))), interpolation=cv.INTER_CUBIC)
-            lidar_range_img = cv.applyColorMap(lidar_range_img, cv.COLORMAP_HSV)
-
-            lidar_img_sequence_list.append(lidar_range_img)
-
-        lidar_total_img.append(cv.hconcat(lidar_img_sequence_list))
-        lidar_img_sequence_list = []
-    
-    final_lidar_img_output = cv.vconcat(lidar_total_img)
-
-    ### Sequential Image Stack Display ###
-    img_sequence_list = []
-    total_img = []
-    seq_len = dataloader.dataset.sequence_length
-    for batch_index in range(current_img_tensor.size(0)):
-
-        for seq in range(dataloader.dataset.sequence_length):
-            current_img = np.array(TF.to_pil_image(current_img_tensor[batch_index][seq]))
-            current_img = cv.cvtColor(current_img, cv.COLOR_RGB2BGR)    # Re-Order the image array into BGR for display purpose
-            current_img = cv.resize(current_img, dsize=(int(1280/seq_len), int(240/(seq_len * 0.5))), interpolation=cv.INTER_CUBIC)
-
-            img_sequence_list.append(current_img)
-
-        total_img.append(cv.hconcat(img_sequence_list))
+        ### Sequential Image Stack Display ###
         img_sequence_list = []
-    
-    final_img_output = cv.vconcat(total_img)
+        total_img = []
+        seq_len = dataloader.dataset.sequence_length
+        for batch_index in range(current_img_tensor.size(0)):
 
-    final_output = cv.vconcat([final_lidar_img_output, final_img_output])
+            for seq in range(dataloader.dataset.sequence_length):
+                current_img = np.array(TF.to_pil_image(current_img_tensor[batch_index][seq]))
+                current_img = cv.cvtColor(current_img, cv.COLOR_RGB2BGR)    # Re-Order the image array into BGR for display purpose
+                current_img = cv.resize(current_img, dsize=(int(1280/seq_len), int(240/(seq_len * 0.5))), interpolation=cv.INTER_CUBIC)
 
-    cv.imshow('3D LiDAR Range Image', final_output)
-    cv.waitKey(1)
+                img_sequence_list.append(current_img)
+
+            total_img.append(cv.hconcat(img_sequence_list))
+            img_sequence_list = []
+        
+        final_img_output = cv.vconcat(total_img)
+
+        cv.imshow('3D LiDAR Range Image', final_img_output)
+        cv.waitKey(1)
