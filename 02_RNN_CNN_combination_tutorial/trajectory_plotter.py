d11f1cb0f9bfc5e118b0b4b13989c89e270c2f37
luwis93choi <luwis93choi@hotmail.com>
2021-05-23 16:55:11 +0900

    Use max value for axis scaling

--- 02_RNN_CNN_combination_tutorial/trajectory_plotter.py
+++ 02_RNN_CNN_combination_tutorial/trajectory_plotter.py
@@ -7,10 +7,16 @@ VO_GT_list = np.loadtxt('./CRNN_groundtruth.txt').reshape(-1, 2)
 
 # print(VO_est_list.shape)
 
+GT_axis_max_lim = np.max(VO_GT_list)
+est_axis_max_lim = np.max(VO_est_list)
+
+axis_max_lim = np.divide(np.max(np.array([GT_axis_max_lim, est_axis_max_lim])), 100)
+
 plt.title('Deep CRNN Visual Odometry Result')
-plt.xlabel('X [cm]')
-plt.ylabel('Z [cm]')
-plt.plot(VO_est_list[:, 0], VO_est_list[:, 1] , color='b', label='VO Pose Estimation')
-plt.plot(VO_GT_list[:, 0], VO_GT_list[:, 1] , color='r', label='VO Groundtruth')
+plt.xlabel('X [m]')
+plt.ylabel('Z [m]')
+plt.xlim(-axis_max_lim, axis_max_lim)
+plt.plot(np.divide(VO_est_list[:, 0], 100), np.divide(VO_est_list[:, 1], 100) , color='b', label='VO Pose Estimation')
+plt.plot(np.divide(VO_GT_list[:, 0], 100), np.divide(VO_GT_list[:, 1], 100) , color='r', label='VO Groundtruth')
 plt.legend()
 plt.show()
